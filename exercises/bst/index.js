f74070f79633bef5e0b769700951864aac8b0964
Koster, Amanda EX1 <akoster@us.imshealth.com>
2019-12-27 15:34:32 -0800

    bst insert and contains

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -14,14 +14,38 @@
 class Node {
   constructor(data) {
     this.data = data;
-    this.children = [];
+    this.left = null;
+    this.right = null;
     }
 
-  add(data){
-    this.children.push(new Node(data));
+  insert(data){
+    if(this.data > data && this.left){
+      this.left.insert(data)
+    } else if(this.data > data){
+      this.left = new Node(data)
+    } else if(this.data < data && this.right){
+      this.right.insert(data)
+    } else if(this.data < data){
+      this.right = new Node(data)
+    }
+  }
+
+  contains(data){
+
+    if(this.data === data){
+      return this
+    }
+
+    if(this.data > data && this.left){
+      return this.left.contains(data)
+      } else if(this.data < data && this.right){
+        return this.right.contains(data)
+        }
+
+    return null
+
   }
 }
 
-// class Tree {}
 
 module.exports = Node;
